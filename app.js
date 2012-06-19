2dc4c684a83962293d332b6ad828af0f9536cb00
Martin Angers <martin.n.angers@gmail.com>
2012-06-19 13:29:11 -0400

    all basic API done, except intervention ID

--- app.js
+++ app.js
@@ -2,11 +2,23 @@
 
 var server = require('./lib/server'),
   handler = require('./lib/handler'),
-  router = require('./lib/router')
+  router = require('./lib/router'),
+  hNames,
+  hFunc
 
 // Apply routes and handlers to the server
 for (var i = 0; i < router.length; i++) {
-  server.get(router[i].path, handler[router[i].handler])
+  if (typeof router[i].handler === 'string') {
+    server.get(router[i].path, handler[router[i].handler])
+  } else {
+    // Array of handlers
+    hNames = router[i].handler
+    hFunc = []
+    for (var j = 0; j < hNames.length; j++) {
+      hFunc.push(handler[hNames[j]])
+    }
+    server.get(router[i].path, hFunc)
+  }
 }
 
 // Start listening
