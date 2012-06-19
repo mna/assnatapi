f4e05668ee40be5c39d278d15ce1c9be31c4c796
Martin Angers <martin.n.angers@gmail.com>
2012-06-19 12:52:23 -0400

    refactor, cleaner separation of concerns

--- app.js
+++ app.js
@@ -1,71 +1,13 @@
 /*jshint asi:true, trailing:true*/
 
-var restify = require('restify'),
-  db = require('./lib/db'),
-  noevil = require('see-no-evil'),
-  util = require('./lib/util'),
-  server
+var server = require('./lib/server'),
+  handler = require('./lib/handler'),
+  router = require('./lib/router')
 
-server = restify.createServer({
-  version: '0.1.0'
-})
-server.use(restify.queryParser())
+// Apply routes and handlers to the server
+for (var i = 0; i < router.length; i++) {
+  server.get(router[i].path, handler[router[i].handler])
+}
 
-server.get('/deputes', function(req, res, next) {
-  var wrap = noevil({
-    error: next
-  })
-
-  // TODO : Whitelist and normalize query string
-  db.deputies.getAll(req.query, wrap(function(nul, deps) {
-    res.send(deps)
-    return next()
-  }))
-})
-
-server.get('/deputes/:idDep', function(req, res, next) {
-  var wrap = noevil({
-    error: next
-  })
-
-  db.deputies.get(req.params.idDep, wrap(function(nul, dep) {
-    if (dep) {
-      res.send(dep)
-    } else {
-      res.send(new restify.ResourceNotFoundError('The requested ID does not exist.'))
-    }
-    return next()
-  }))
-})
-
-server.get('/deputes/:idDep/interventions', function(req, res, next) {
-  var wrap = noevil({
-    error: next
-  }), opts
-
-
-  // TODO : Whitelist and normalize query string
-  opts = util.extend({}, req.query, {deputyId: req.params.idDep})
-
-  db.interventions.getAll(opts, wrap(function(nul, ints) {
-    res.send(ints)
-    return next()
-  }))
-})
-
-server.get('/deputes/:idDep/interventions/:idInt', function(req, res, next) {
-  var wrap = noevil({
-    error: next
-  })
-
-  db.interventions.get(req.params.idInt, wrap(function(nul, intr) {
-    if (intr) {
-      res.send(intr)
-    } else {
-      res.send(new restify.ResourceNotFoundError('The requested ID does not exist.'))
-    }
-    return next()
-  }))
-})
-
+// Start listening
 server.listen(3000)
