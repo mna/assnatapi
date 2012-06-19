44398d3c31789dcf62ec56d4e7e2a5b3160df940
Martin Angers <martin.n.angers@gmail.com>
2012-06-19 14:22:43 -0400

    minor refactoring to setup routes

--- app.js
+++ app.js
@@ -2,24 +2,10 @@
 
 var server = require('./lib/server'),
   handler = require('./lib/handler'),
-  router = require('./lib/router'),
-  hNames,
-  hFunc
+  router = require('./lib/router')
 
-// Apply routes and handlers to the server
-for (var i = 0; i < router.length; i++) {
-  if (typeof router[i].handler === 'string') {
-    server.get(router[i].path, handler[router[i].handler])
-  } else {
-    // Array of handlers
-    hNames = router[i].handler
-    hFunc = []
-    for (var j = 0; j < hNames.length; j++) {
-      hFunc.push(handler[hNames[j]])
-    }
-    server.get(router[i].path, hFunc)
-  }
-}
+// Setup the routes
+server.setupRoutes(router, handler)
 
 // Start listening
 server.listen(3000)
