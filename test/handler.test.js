8b7cb9c74b22db6cb6d5dd6c6e83dc769e51afd6
Martin Angers <martin.n.angers@gmail.com>
2012-06-22 21:47:45 -0400

    start testing handlers

--- test/handler.test.js
+++ test/handler.test.js
@@ -2,8 +2,10 @@ var expect = require('expect.js'),
   db = {
 
   },
-  sut = require(process.env.ASSNAT_COV ? '../lib-cov/handler' : '../lib/handler')
+  sut = require(process.env.ASSNAT_COV ? '../lib-cov/handler' : '../lib/handler')(db)
 
 describe('handler', function() {
-  
+  it('should expose an object', function() {
+    expect(sut).to.be.an('object')
+  })
 })
